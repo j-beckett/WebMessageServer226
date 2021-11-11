f7dcc2a8f4785535c5d89e0f03c73f5216b4448b
Jen Beckett ICS 226 <jennybeckett2@hotmail.com>
2021-11-10 16:47:50 -0800

    final for this lab, with comments and error check

--- client.py
+++ client.py
@@ -12,20 +12,29 @@ SERVER_PORT = sys.argv[2]
 GET_URL = "http://" + SERVER_HOST + ':' + SERVER_PORT + '/' + "msgserver/get/"
 POST_URL = "http://" + SERVER_HOST + ':' + SERVER_PORT + '/' + "msgserver/create/"
 KEY_LENGTH = 8
+MAX_MESSAGE_LENGTH = 160
+
+MESSAGE_KEY_START = 10
+MESSAGE_KEY_END = 18  
 
 #takes no params, generates a random key for the next message in the linked list
 # get random key of length KEY_LENGTH ( 8 ) with letters(upperCase & lowercase) + digits
 def generateKey():
     characters = string.ascii_letters + string.digits
-    randomKey = ''.join(random.choice(characters) for i in range(KEY_LENGTH)) #plus one to ensure we get the full 8 chars
+    randomKey = ''.join(random.choice(characters) for i in range(KEY_LENGTH))
     #print("Random password is:", randomKey)
 
     return randomKey
 
-#this function prompts user for input. DOES ERROR CHECKING ######################################################
+#this function prompts user for input. DOES ERROR CHECKING on usrInput and enforces the proper message length.
+#Returns a VALID string to be saved into the message dictionary
 def promptForMessage():
     print("Please enter a new message: \n")
     usrString = input()
+
+    while len(usrString ) < 1 or len(usrString ) > MAX_MESSAGE_LENGTH:
+        print("Invalid message size. Please input a message longer than 0 and less than " + str(MAX_MESSAGE_LENGTH) + "\n")
+        usrString = input()
 
     return usrString
 
@@ -45,12 +54,12 @@ def createConnection(message_key, messageToSend):
     if 'csrftoken' in client.cookies:
         elements = {'message':messageToSend, 'key':message_key, 'csrfmiddlewaretoken':client.cookies['csrftoken']}
         post = client.post(POST_URL, data = elements, headers = {'Referer' : POST_URL})
-#close the connection?
+
 
 
 #this function is the 'main' runner of the program. 
 #takes in no params, however gets vars off the command line using sys.argv. 
-#e                                                                               ###################check 4 invlaid input
+#                                                                                    ###################check 4 invlaid input
 
 #if there is no messages for a user provided key, starts a new message 'thread' (think reddit thread, not a multithreading thread)
 #the message thread is implemented with a link list of keys. Each message containing the pointer to the next message in the first KEY_LENGTH (8) chars of the message
@@ -59,6 +68,10 @@ def createConnection(message_key, messageToSend):
 def client():
     try:
         message_key = sys.argv[3]
+        if len(message_key) != KEY_LENGTH or message_key.isalnum() == False:
+            print("Key length must be exactly " + str(KEY_LENGTH) + " chars and alphanumeric. Please try again.")
+            sys.exit(-2)
+
         while(True): #Loop until a "No message at key" message is found. If found, break the loop and prompt user to enter a new message.
             response = requests.get(GET_URL + message_key + '/')
             data = response.text
@@ -66,8 +79,8 @@ def client():
             if (data == "No message at key"+ message_key):
                 break
 
-            print(data[17: ])
-            message_key = (data[10 : 18])
+            print(data[MESSAGE_KEY_END - 1: ]) #the actual message starts after the key included in the message. this should account for changes in key length 
+            message_key = (data[MESSAGE_KEY_START : MESSAGE_KEY_END])
             
             
         newUsrMessage = promptForMessage()
